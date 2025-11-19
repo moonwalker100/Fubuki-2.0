777bf30c7c2838bda114f7ca3cd73594ed21e3dc
{remo} <moonwalker1092@gmail.com>
2025-11-19 23:59:42 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -7,9 +7,13 @@ from database.database import add_user, del_user, full_userbase, present_user
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
+
     if data == "help":
         await query.message.edit_text(
-            text=HELP_TXT.format(first=query.from_user.first_name),
+            text=HELP_TXT.format(
+                first=query.from_user.first_name,
+                botname=client.me.first_name
+            ),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
@@ -20,30 +24,50 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 ]
             )
         )
+
     elif data == "about":
         await query.message.edit_text(
-            text=ABOUT_TXT.format(first=query.from_user.first_name),
+            text=ABOUT_TXT.format(
+                first=query.from_user.first_name,
+                botname=client.me.first_name
+            ),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
-                     InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')]
+                    [
+                        InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
+                        InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')
+                    ]
                 ]
             )
         )
+
     elif data == "start":
         await query.message.edit_text(
-            text=START_MSG.format(first=query.from_user.first_name),
+            text=START_MSG.format(
+                first=query.from_user.first_name,
+                botname=client.me.first_name
+            ),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
-                 InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data='about')],
-                [InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')]
+                [
+                    InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
+                    InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data='about')
+                ],
+                [
+                    InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')
+                ]
             ])
         )
+
     elif data == "premium":
         await query.message.edit_text(
-            text=f"Premium Benefits & Perks\nDirect Channel Links, No Ad Links\nSpecial Access In Events\n\n<blockquote>Pricing Rates\n7 Days - INR 39\n15 Days - INR 69\n1 Month - INR 89\n3 Months - INR 249\n6 Months - INR 479\n12 Month - Contact <a href=https://t.me/WhiteBeard_Sama>Owner</a></blockquote>\n\n<blockquote>Want To Buy?\nUsing UPI 8527282574@ybl\nSend Screenshot to <a href=https://t.me/WhiteBeard_Sama>Owner</a></blockquote>\n\nWe Have Limited Seats For Premium Users",
+            text=f"Premium Benefits & Perks\nDirect Channel Links, No Ad Links\nSpecial Access In Events\n\n"
+                 f"<blockquote>Pricing Rates\n7 Days - INR 39\n15 Days - INR 69\n1 Month - INR 89\n3 Months - INR 249\n"
+                 f"6 Months - INR 479\n12 Month - Contact <a href=https://t.me/WhiteBeard_Sama>Owner</a></blockquote>\n\n"
+                 f"<blockquote>Want To Buy?\nUsing UPI 8527282574@ybl\nSend Screenshot to "
+                 f"<a href=https://t.me/WhiteBeard_Sama>Owner</a></blockquote>\n\n"
+                 f"We Have Limited Seats For Premium Users",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
